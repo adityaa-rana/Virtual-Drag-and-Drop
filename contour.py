c64a4fd3837aec69822508280fae2f49586890a1
Aditya Rana <174363179+adityaa-rana@users.noreply.github.com>
2025-06-15 20:44:01 +0530

    First pus

--- contour.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-img=cv.imread('./cat.webp')
-
-grayimg=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-cv.imshow("Gray",grayimg)
-cannyimg=cv.Canny(img,125,175 )
-cv.imshow("Canny",cannyimg)
-
-contours,heirarchies=cv.findContours(cannyimg,cv.RETR_LIST,cv.CHAIN_APPROX_NONE)
